2cb794e5c564acc580aa9e18dda1223ca0c35458
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-10-28 10:46:20 -0300

    chore: adjust values

--- src/constants.h
+++ src/constants.h
@@ -8,10 +8,10 @@ constexpr int TARGET_FRAME_TIME = 1000 / FPS;
 
 constexpr float UNIT = 100.0f;
 
-constexpr float GRAVITY_FORCE = 6.0f * UNIT;
+constexpr float GRAVITY_FORCE = 5.0f * UNIT;
 
-constexpr float MAX_FLY_SPEED = 10.0f * UNIT;
+constexpr float MAX_FLY_SPEED = 3.0f * UNIT;
 constexpr float MAX_FALL_SPEED = 10.0f * UNIT;
-constexpr float FLY_ACCELERATION = 10.0f * UNIT;
+constexpr float FLY_ACCELERATION = 7.0f * UNIT;
 
 constexpr int FLOOR_Y = WINDOW_HEIGHT - 48;
