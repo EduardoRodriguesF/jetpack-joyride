6258e12fd71c65db9fe8a1a3ef74b72fc3a43099
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-10-29 22:41:01 -0300

    chore: adjust values

--- src/constants.h
+++ src/constants.h
@@ -19,5 +19,5 @@ constexpr int FLOOR_Y = WINDOW_HEIGHT - 48;
 #define SCENE_ACCELERATION 0.001f * UNIT;
 
 #define ROCKET_LAUNCH_TIME 1000
-constexpr float ROCKET_SPEED = 5.0f * UNIT;
-constexpr float ROCKET_AIM_SPEED = 0.8f * UNIT;
+constexpr float ROCKET_SPEED = 7.0f * UNIT;
+constexpr float ROCKET_AIM_SPEED = 1.3f * UNIT;
